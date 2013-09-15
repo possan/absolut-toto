20415fd7a60a77f24baabadf2efbc24004155d72
Per-Olov Jernberg <possan@possan.se>
2013-09-15 15:31:39 +0200

    bartender code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var g_queue = [];
 var g_index = 0;
 var g_allsockets = [];
+var g_bartenders = {};
 
 var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
@@ -36,17 +37,16 @@ function handler (req, res) {
   );
 }
 
-function broadcast(event) {
+function broadcast(eventname, eventdata) {
   g_allsockets.forEach(function(socket) {
-    socket.emit('queue-update', event);
+    socket.emit(eventname, eventdata);
   });
 }
 
 function broadcast_queue() {
-  g_allsockets.forEach(function(socket) {
-    socket.emit('queue-update', {
-      queue: g_queue
-    });
+  broadcast('queue-update', {
+    queue: g_queue,
+    bartenders: g_bartenders
   });
 }
 
@@ -64,22 +64,67 @@ io.sockets.on('connection', function (socket) {
     console.log('disconnected', id);
     var idx = g_allsockets.indexOf(socket);
     delete(g_allsockets[idx]);
-  });
-
-  socket.on('my other event', function (data) {
-    console.log(data);
+    if (id != '') {
+      var idx = g_queue.indexOf(id);
+      if (idx != -1) {
+        broadcast('queue-remove', { place: pos, id: data.id });
+      }
+    }
   });
 
   socket.on('button-down', function (data) {
     console.log('button-down', data);
-    g_queue.push('+'+data.id);
+    var pos = g_queue.length;
+    g_queue.push(data.id);
     broadcast_queue();
+    broadcast('queue-add', { place: pos, id: data.id });
   });
 
   socket.on('button-up', function (data) {
     console.log('button-up', data);
-    g_queue.push('-'+data.id);
+
+    // if in use by bartender, ignore button-up.
+
+    var in_progress = false;
+    var key;
+    while(key in g_bartenders) {
+      if (g_bartenders[key] == data.id)
+        in_progress = true;
+    }
+
+    if (in_progress)
+      return;
+
+    var idx = g_queue.indexOf(data.id);
+    if (idx != -1) {
+      g_queue.splice(idx, 1);
+      // delete(g_queue[idx]);
+    }
+
     broadcast_queue();
+  });
+
+  socket.on('bartender-serve', function (data) {
+    // console.log('button-up', data);
+    // g_queue.push('-'+data.id);
+    g_bartenders[data.bartender] = data.customer;
+    broadcast_queue();
+    broadcast('bartender-serve', { bartender: data.bartender, customer: data.id });
+  });
+
+  socket.on('bartender-done', function (data) {
+    // console.log('button-up', data);
+    // g_queue.push('-'+data.id);
+    if (g_bartenders[data.bartender]) {
+      var customer = g_bartenders[data.bartender];
+      delete(g_bartenders[data.bartender]);
+      var idx = g_queue.indexOf(customer);
+      if (idx != -1) {
+        g_queue.splice(idx, 1);
+      }
+    }
+    broadcast_queue();
+    broadcast('bartender-done', { bartender: data.bartender });
   });
 
   socket.on('get-button-id', function (data) {
@@ -90,10 +135,6 @@ io.sockets.on('connection', function (socket) {
     console.log('assign id', id);
     socket.emit('button-id', { id: id });
     broadcast_queue();
-  });
-
-  socket.on('my other event', function (data) {
-    console.log(data);
   });
 
 });
