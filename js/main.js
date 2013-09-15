7e1919651071b00c5140bfba7427967bfc646520
Andreas Löf Hermansson <andreas.lof@kurppahosk.com>
2013-09-15 17:52:25 +0200

    Merging and slashing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+function drawElements() {
+  d3svg.selectAll('circle').data(c_queue).enter().append('circle')
+  .attr('cx', function (d) { return 50 * d; })
+  .attr('cy', 30)
+  .attr('r', 0)
+  .transition()
+  .attr('r', 25);
+}
