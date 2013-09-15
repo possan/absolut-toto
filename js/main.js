84e8bf7c61b3997eea62a244c1c1120d626ee00c
Andreas Löf Hermansson <andreas.lof@kurppahosk.com>
2013-09-15 18:17:54 +0200

    checking in some minor fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function drawElements() {
   d3svg.selectAll('circle').data(c_queue).enter().append('circle')
-  .attr('cx', function (d) { return 50 * d; })
+  .attr('cx', function (d) { return (50 * c_queue.indexOf(d)) + 25; })
   .attr('cy', 30)
   .attr('r', 0)
   .transition()
