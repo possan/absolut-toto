6fdf66c095ec1964c0ad37f6beda4fa09d80921b
Andreas Löf Hermansson <andreas.lof@kurppahosk.com>
2013-09-15 21:13:59 +0200

    some colours and so

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-function drawElements() {
-  d3svg.selectAll('circle').data(c_queue).enter().append('circle')
-  .attr('cx', function (d) { return (50 * c_queue.indexOf(d)) + 25; })
-  .attr('cy', 30)
-  .attr('r', 0)
+function tick() {
+
+}
+
+function removeElements(size) {
+  d3svg.selectAll('circle').data(c_queue)
+  .exit()
   .transition()
-  .delay(function (d, i) { return 10 * i; })
-  .attr('r', 25);
+  .duration(120)
+  .attr('r', size+(size/6))
+  .attr('alpha', 0)
+  .transition()
+  .duration(180)
+  .attr('r', 0).remove();
+}
+
+function drawElements(size) {  
+  var 
+  colors = [
+    "#FF9EA0",
+    "#A99DFF",
+    "#FF94FF",
+    "#7FFF9F"
+  ],
+  circles = d3svg.selectAll('circle').data(c_queue);
+
+  circles.enter().append('circle')
+  .attr('cx', function (d, i) { return (125 * i) + (size + 25); })
+  .attr('cy', 125)
+  .attr('r', 0)
+  .attr('fill', function (d, i) { return colors[i]; })
+  .transition()
+  .duration(180)
+  .attr('r', size);
 }
