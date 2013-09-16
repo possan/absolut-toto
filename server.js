f580b27ff757ed01e3ea74338c863a84a875024b
Tor Castensson <tor@plan8.se>
2013-09-16 11:26:40 +0200

    no message

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ var app = require('http').createServer(handler),
     fs = require('fs')
     //osc = require('node-osc');
 
-io.configure(function () { 
+/*io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
-});
+});*/
 
 //var oscclient = new osc.Client('127.0.0.1', 3333);
 
-app.listen(80);
+app.listen(8080);
 
 function handler (req, res) {
   console.log('req.url', req.url);
