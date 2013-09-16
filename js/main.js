fc4aba6a715b19cd5237f9dc177ab421af72bcd9
Andreas Löf Hermansson <andreas.lof@kurppahosk.com>
2013-09-16 10:02:43 +0200

    fixes fixes fixes

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
-function removeElements(size) {
-  d3svg.selectAll('circle').data(c_queue)
-  .exit()
-  .transition()
-  .duration(120)
-  .attr('r', size+(size/6))
-  .attr('alpha', 0)
-  .transition()
-  .duration(180)
-  .attr('r', 0).remove();
+function removeElements(data) {
+  $('#render').find('.no_'+data.id).remove();
 }
 
-function drawElements(size) {  
+function drawElements(data) {  
   var 
   colors = [
     "#FF9EA0",
     "#A99DFF",
     "#FF94FF",
     "#7FFF9F"
-  ],
-  circles = d3svg.selectAll('circle').data(c_queue);
+  ];
 
-  circles.enter().append('circle')
-  .attr('cx', function (d, i) { return (125 * i) + (size + 25); })
-  .attr('cy', 125)
-  .attr('r', 0)
-  .attr('fill', function (d, i) { return colors[i]; })
-  .transition()
-  .duration(180)
-  .attr('r', size);
+  var elem = document.createElement('div');
+  elem = $(elem);
+  elem.addClass('player no_'+data.id);
+  elem.html('<p>Player<br>#'+data.id+'</p>');
+  elem.css({
+    'background-color': colors[data.id%colors.length]
+  });
+  elem.appendTo($('#render'));
+  elem.addClass('show');
 }
